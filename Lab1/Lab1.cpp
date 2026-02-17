b9e08b36a5e0af0f8d2a498deccf35ae0c44118c
Shishkov Egor <131530870+AnimeMakartur@users.noreply.github.com>
2026-02-17 12:18:18 +0200

    Update

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -2,78 +2,66 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+//Константи для обмеження кількості цифр у випадковому числі
+#define MinN 15
+#define MaxN 30
 
-int inputNumb();
-char* createNumb(int, char[]);
-void createArrayOfNums(int, char[], char*[]);
-int sumOfDigits(char*);
-void printArrayAndSums(int, char* []);
+//Прототипи функцій
+void inputNumber(int*);//Введення кількості цифр
+char* createNumb(int, char[]);//Створення випадкового числа у вигляді рядка
+void createArrayOfNums(int, char[], char* []);//Створення масиву рядків з випадковими числами
+int sumOfDigits(char*);//Підрахунок суми цифр у рядку
+void printArrayAndSums(int, char* []);//Виведення масиву рядків та суми цифр у кожному рядку
 
+//Головна функція
 int main()
 {
-	char num[31];
-	char* arr[30];
-	int N = inputNumb();
+	char num[MaxN + 1];
+	char* arr[MaxN];
+	int N;
+	inputNumber(&N);
 	srand(time(0));
 	createArrayOfNums(N, num, arr);
 	printArrayAndSums(N, arr);
 	return 0;
 }
 
-int inputNumb()
+void inputNumber(int* Numb)
 {
-	int N;
 	printf("Enter the number of digits: ");
-	label:
-	scanf_s("%d", &N);
-	if(N < 14 || N > 31)
+lable:
+	scanf_s("%d", Numb);
+	if (*Numb < MinN || *Numb > MaxN)
 	{
-		printf("Invalid input. Please enter a number between 15 and 30: ");
-		goto label;
+		printf("Invalid input. Please enter a number between 15 and 30\n");
+		goto lable;
 	}
-	return N;
 }
-
 char* createNumb(int N, char num[])
 {
-	char* i = num;
-	for (; i < num+N; i++)
-		*i = rand() % 10 + '0';
-	*i = '\0';
+	*num = rand() % 9 + 1 + '0';//Генерація першої цифри від 1 до 9, щоб уникнути провідних нулів
+	char* pNum = num + 1;
+	for (; pNum < num + N; pNum++)
+		*pNum = rand() % 10 + '0';
+	*pNum = '\0';
 	return num;
 }
-
 void createArrayOfNums(int N, char num[], char* arr[])
 {
-	char** i = arr;
-	for(; i < arr+ N; i++)
-	{
-		*i = _strdup(createNumb(N, num));
-	}
-
+	for (char** parr = arr + N; arr < parr; arr++)
+		*arr = _strdup(createNumb(N, num));//_strdup виділяє пам'ять для рядка та копіює його
 }
-
 int sumOfDigits(char* num)
 {
 	int sum = 0;
-	char* i = num;
-	while (*i++)
-	{
-		sum += *i - '0';
-	}
+	while (*num)
+		sum += *num++ - '0';
 	return sum;
 }
-
 void printArrayAndSums(int N, char* arr[])
 {
-	char** i = arr;
-
-	printf("\n%-32s | %s\n", "Generated Number", "Sum of Digits");
-	printf("----------------------------------------------------------\n");
-
-	for (; i < arr + N; i++)
-	{
-		printf("%-32s | %d\n", *i, sumOfDigits(*i));
-	}
+	printf("\n%-32s | %s\n", "Generated Number", "Sum of Digits");//Виведення заголовку таблиці
+	printf("----------------------------------------------------------\n");//Виведення рядків та їх сум у вигляді таблиці
+	for (char** parr = arr + N; arr < parr; arr++)
+		printf("%-32s | %d\n", *arr, sumOfDigits(*arr));//Виведення ря-дків та їх сум у вигляді таблиці
 }
-
